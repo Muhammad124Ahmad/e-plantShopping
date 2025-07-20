f6a775d5ddc683823c9ff7a999524a2598ecd868
muhammad1249ahmad <>
2025-07-20 19:02:49 +0500

    Added some changes in vite.config.js for deployement

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  base: '/e-plantShopping/', // <-- your repo name goes here
 })

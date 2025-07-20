29f1e8e31e867b51f131b6847b69af4789e720a8
muhammad1249ahmad <>
2025-07-20 16:06:55 +0500

    Setup the Cartslice and expored it's reducer and actions.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const itemToBeAdded = action.payload;
+      if (itemToBeAdded) {
+        state.items.push({...itemToBeAdded,  quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const itemToBeRemoved = action.payload;
+      const isFound = state.items.findIndex(
+        (item) => item.name == itemToBeRemoved.name
+      );
+      if (isFound!=-1) {
+        state.items.splice(isFound, 1);
+      }
     },
     updateQuantity: (state, action) => {
-
-    
+      const { itemToUpdate, amount } = action.payload;
+      const isFound = state.items.findIndex(
+        (item) => item.name == itemToUpdate.name
+      );
+      if (isFound != -1) {
+        state.items[isFound].quantity =amount;
+      }
     },
   },
 });
